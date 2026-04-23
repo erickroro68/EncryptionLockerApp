6c94ec2cab690de87b552277a15ae24102b40ad1
erickroro68 <36974044+erickroro68@users.noreply.github.com>
2026-04-23 08:10:45 -0500

    Set up clean Evidence Locker structure

---
+++ encrytlock/README.md
@@ -0,0 +1,3 @@
+Project: Evidence Locker System  
+A Java project demonstrating clean OOP design, service‑based architecture, file encryption, expiration logic, and audit logging.
+
---
+++ encrytlock/src/main/java/com/locker/App/EvidenceLockerApp.java
@@ -0,0 +1,5 @@
+package com.locker.App;
+public class EvidenceLockerApp {
+    // this class will be our app classthat handles our menu starting up our steps
+    // from other classes where we made new objects to use
+}
